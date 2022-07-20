a7ab0fb15d2546e18106e8479e8f6096f17fb213
Joe <lolkiller1@hotmail.com>
2022-07-20 12:57:01 -0400

    testing nodejs external libs

---
+++ clock.js
@@ -0,0 +1,8 @@
+export default class Clock {
+	var num = 0;
+	
+    getNum() {
+		num = num + 1;
+        return num;
+    }
+}
--- index.js
+++ index.js
@@ -3,18 +3,21 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
+import Clock from './clock.js';
+const clock = new Clock();
 
-def homePage = function(res) {
-	res.send('<h1>Home</h1>');
+var homePage = function(req, res) {
+	res.send('<h1>Home</h1><p>Clock says ' + clock.getNum() + '</p>');
+	console.log("Sent home to " + req.ip);
 }
 
 app.get('/', (req, res) => {
-	homePage(res);
+	homePage(req, res);
 });
 
 app.get('/home', (req, res) => {
-	homePage(res);
-}
+	homePage(req, res);
+});
 
 server.listen(3000, () => {
 	console.log('listening on *:3000');
