065c4e86c788abe9814aad913887563bad0cd838
Joe <lolkiller1@hotmail.com>
2022-07-20 11:51:40 -0400

    testing website paths

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
+
+def homePage = function(res) {
+	res.send('<h1>Home</h1>');
+}
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+	homePage(res);
 });
 
+app.get('/home', (req, res) => {
+	homePage(res);
+}
+
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
